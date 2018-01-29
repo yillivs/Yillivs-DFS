b365f09c0efd731e68edffa71a1119cdeab30e31
Oliver Dennis Sanchez <oliver.d.sanchez@wmich.edu>
2018-01-28 19:32:07 -0500

    I actually saved before I committed this time :S

--- hw#cs3310_Sanchez_013018/src/com/company/Item.java
+++ hw#cs3310_Sanchez_013018/src/com/company/Item.java
@@ -1,5 +1,8 @@
 package com.company;
 
+/**
+ *
+ */
 public class Item {
     protected String name;
     protected int    minStr;
@@ -7,42 +10,93 @@ public class Item {
     protected String rarity;
     protected int    currStr;
 
+    /**
+     *
+     */
+    public Item(){
+        this.name = null;
+        this.minStr = 0;
+        this.maxStr = 0;
+        this.rarity = null;
+        this.currStr = 0;
+    }
+
+    /**
+     *
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getMinStr() {
         return minStr;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getMaxStr() {
         return maxStr;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getRarity() {
         return rarity;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getCurrStr() {
         return currStr;
     }
 
+    /**
+     *
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     *
+     * @param minStr
+     */
     public void setMinStr(int minStr) {
         this.minStr = minStr;
     }
 
+    /**
+     *
+     * @param maxStr
+     */
     public void setMaxStr(int maxStr) {
         this.maxStr = maxStr;
     }
 
+    /**
+     *
+     * @param rarity
+     */
     public void setRarity(String rarity) {
         this.rarity = rarity;
     }
 
+    /**
+     *
+     * @param currStr
+     */
     public void setCurrStr(int currStr) {
         this.currStr = currStr;
     }
--- hw#cs3310_Sanchez_013018/src/com/company/Main.java
+++ hw#cs3310_Sanchez_013018/src/com/company/Main.java
@@ -1,8 +1,93 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
+    public static final int LINECONST = 750;
+
+    /**
+     *
+     * @param fp
+     * @return
+     */
+    public static Item[] readArray(String fp){
+        Item[] list = new Item[LINECONST];
+
+        String line = null;
+        int counter = 0;
+
+        try
+        {
+            FileReader freader = new FileReader(fp);
+            BufferedReader buffReader = new BufferedReader(freader);
+
+            buffReader.readLine();
+            while((line = buffReader.readLine()) != null)
+            {
+                int fieldCount = 0;
+                if(line.trim().length() > 0)
+                {
+                    String[] split = line.split(",");
+                    Item lineItem = new Item();
+                    lineItem.setName(split[0]);
+                    if(split[1].equalsIgnoreCase(""))
+                    {
+                        lineItem.setMinStr(0);
+                    }
+                    else
+                    {
+                        lineItem.setMinStr(Integer.parseInt(split[1]));
+                    }
+                    if(split[2].equalsIgnoreCase(""))
+                    {
+                        lineItem.setMaxStr(0);
+                    }
+                    else
+                    {
+                        lineItem.setMaxStr(Integer.parseInt(split[2]));
+                    }
+                    lineItem.setRarity(split[3]);
+                    list[counter] = lineItem;
+                    counter++;
+                }
+            }
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("File not found");
+        }
+        catch(IOException e)
+        {
+            System.out.println("IOException");
+        }
+        return list;
+    }
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-	// write your code here
+        String filepath = "weapons.txt";
+        Item[] populatedList = readArray(filepath);
+
+        System.out.println("Please enter how many bags you would like to create.");
+        Scanner kb = new Scanner(System.in);
+
+        int bagSize = 0;
+        //Ensures the newline is consumed.
+        try{
+            bagSize = Integer.parseInt(kb.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Provided bag size is not in integer format");
+        }
+
+        Bag[] userBags = new Bag[bagSize];
+
     }
 }
